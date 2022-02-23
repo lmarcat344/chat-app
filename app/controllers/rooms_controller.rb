d5224e41545bc82350d3dd37fc211d2867140461
lmarcat344 <lmarcat344@gmail.com>
2022-02-23 16:58:12 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,11 @@
 class RoomsController < ApplicationController
+  def index
+    room_temp = RoomUser.where(user_id: current_user.id)
+    @room = []
+    room_temp.each do |room|
+      @room << Room.find(room.room_id)
+    end
+  end
   def new
     @room = Room.new()
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users
   # resources :users, only: :edit             ##view/edit.html.erbも変える
   # get '/users/:id/edit', to: 'users#edit'   ##同上
