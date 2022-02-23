985cd8fc694f7ade5de64346c61888d7bc41ef63
lmarcat344 <lmarcat344@gmail.com>
2022-02-23 09:53:08 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users
-  # resources :users, only: :edit
-  # get '/users/:id/edit', to: 'users#edit'
+  # resources :users, only: :edit             ##view/edit.html.erbも変える
+  # get '/users/:id/edit', to: 'users#edit'   ##同上
+  resources :rooms
 end
